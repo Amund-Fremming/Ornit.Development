6c9af39bc9ace9b3ce75606e66de60442d232813
Amund-fremming <amund.fremming@gmail.com>
2025-01-02 18:55:17 +0100

    changed to only be

--- Projects/ReactNative/src/shared/constants/Colors.ts
+++
@@ -1,7 +0,0 @@
-export const Colors = {
-  Black: "black",
-  White: "white",
-  Gray: "gray",
-  Red: "red",
-  Green: "green",
-};
--- Projects/ReactNative/src/shared/constants/endpoints.ts
+++
@@ -1,3 +0,0 @@
-const baseUrl = "";
-
-export const authUrl = `${baseUrl}/auth`;
